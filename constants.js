3b00c73d06d5b462c93206054f3c27b84c0610ea
Shravan Lal <shravanlal98@gmail.com>
2018-04-08 15:24:10 +1000

    raw path

--- constants.js
+++ constants.js
@@ -1,9 +1,9 @@
 const production = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    PUBLIC_PATH: production ? "https://github.com/heartworm/echo720/raw/master/dist/" : "http://localhost:8080/dist/",
+    PUBLIC_PATH: production ? "https://raw.githubusercontent.com/heartworm/echo720/master/dist/" : "http://localhost:8080/dist/",
     RANDOM_VERSION: Date.now().toString(),
-    CONNECT_DOMAIN: production ? "github.com" : "localhost",
+    CONNECT_DOMAIN: production ? "raw.githubusercontent.com" : "localhost",
     MAIN_SCRIPT: "main.js",
     MAIN_STYLES: "main.css"
 };
